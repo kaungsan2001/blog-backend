213a86fac99f42900cf5b6ffc7ca2456ba5fd6b3
kaungsan2001 <kaungsan2001.dev@gmail.com>
2026-04-01 14:22:30 +0630

    fix:fixing bug cookie not sending in deployment

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,33 +1,33 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-// import { apiKey } from "@better-auth/api-key";
+import { apiKey } from "@better-auth/api-key";
 import { prisma } from "../db";
-// import redis_client from "./redis";
+import redis_client from "./redis";
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
-  // secondaryStorage: {
-  //   get: async (key) => {
-  //     return await redis_client.get(key);
-  //   },
-  //   set: async (key, value, ttl) => {
-  //     if (ttl) await redis_client.set(key, value, { EX: ttl });
-  //     else await redis_client.set(key, value);
-  //   },
-  //   delete: async (key) => {
-  //     await redis_client.del(key);
-  //   },
-  // },
-  // session: {
-  //   storeSessionInDatabase: true,
-  // },
-  // plugins: [
-  //   apiKey({
-  //     storage: "secondary-storage",
-  //     fallbackToDatabase: true,
-  //   }),
-  // ],
+  secondaryStorage: {
+    get: async (key) => {
+      return await redis_client.get(key);
+    },
+    set: async (key, value, ttl) => {
+      if (ttl) await redis_client.set(key, value, { EX: ttl });
+      else await redis_client.set(key, value);
+    },
+    delete: async (key) => {
+      await redis_client.del(key);
+    },
+  },
+  session: {
+    storeSessionInDatabase: true,
+  },
+  plugins: [
+    apiKey({
+      storage: "secondary-storage",
+      fallbackToDatabase: true,
+    }),
+  ],
   emailAndPassword: {
     enabled: true,
   },
